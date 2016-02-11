d3bdd12c84ee399478adaf6f08d152790e3f42c8
Edivan de Castro <edivandecastro@gmail.com>
2016-02-11 15:10:23 -0300

    Implemented script for download of HQ

---
+++ downloadhq.rb
@@ -0,0 +1,31 @@
+require 'rubygems'
+require 'mechanize'
+
+print "Enter with URL: [http://hqcomicsonline.com.br/leitor/hq] "
+url = gets.chomp
+url = "http://hqcomicsonline.com.br/leitor/hq" if url.empty?
+
+print "Enter with name of HQ: "
+hq = gets.chomp
+
+print "Enter with list of chapters: ex. 1,2,3 >> "
+chapters = eval("[#{gets.chomp}]")
+
+chapters.each do |chapter|
+  agent = Mechanize.new
+  pag = 0
+  while true
+    pag = pag.succ
+    pag = pag.to_s.rjust(2, "0") if pag.to_s.size == 1
+    chapter = chapter.to_s.rjust(2, "0") if chapter.to_s.size == 1
+    link = "#{url}/#{hq}/#{chapter}/#{pag}.jpg"
+    begin
+      agent.get(link).save "images/#{hq}/chapter #{chapter}/#{pag}.jpg"
+      puts "Download #{link}"
+    rescue Mechanize::ResponseCodeError => e
+      break
+    end
+  end
+  puts "Salvo com Sucesso!"
+end
+
